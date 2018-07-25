8ffcf4d2ae530cb6fc317ddff01b8bc6c95203f7
WangWang <luckboystudent@gmail.com>
2018-07-25 23:49:03 +0800

    first success

--- service.js
+++ service.js
@@ -1,8 +1,43 @@
-let bonjour=require('bonjour')();
-//bonjour.publish({name: 'test', type: 'http',port: 3000 });
+//const bonjour=require('bonjour')();
+const mdns=require('multicast-dns')();
+const txt=require('dns-txt')();
+//bonjour.publish({name: 'test123', type: '_Wong',port: 3000 });
 
+mdns.on('query',function(query){
+	//console.log(query);
+//	let service="_PercomTest._udp.local";
+//	let serviceInstance="PercomTest";
+	mdns.respond({
+		answers: [{
+			name: '_test._udp.local', ttl: 10, type: 'PTR', data: 'test._test._udp.local'
+		},{
+			name: '_services._dns-sd._udp.local', ttl: 10, type: 'PTR', data: '_test._udp.local'
+		}],
+		additionals:[{
+			
+			name:'test._test._udp.local',
+			type:'TXT',
+			ttl:10,
+			data:txt.encode({x:"testewrwer"})
 
-bonjour.find({type:'http'},function(service){
-	console.log('Found an http server:',service);
+		},{
+			name:'test._test._udp.local',
+			type:'SRV',
+			ttl:10,
+			flush:true,
+			data:{
+				port:9900,
+				target:'test.local'
+			}
+		},{
+			name:'test.local',
+			type:'A',
+			ttl:10,
+			flush:true,
+			data:'192.168.4.220'
+
+		}]
+
+	});
 });
 
