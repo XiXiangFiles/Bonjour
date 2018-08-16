f89dfca4b78ccf870fa7624bb2f68ba3daa2d67c
WangWang <luckboystudent@gmail.com>
2018-08-16 17:18:16 +0800

    20180816

--- client.js
+++ client.js
@@ -7,6 +7,7 @@ function queryObj(name,type){
 	obj.type=type;
 	return obj;
 }
+
 function query(name,type){
 	let questions=[];
 	if(typeof(name)==="string" && typeof(type)==="string"){
@@ -25,11 +26,13 @@ function query(name,type){
 	}
 	
 }
+
 function parseSRVTXT(packet){
 	
 	let obj={};
+//	console.log(packet.answers);
 	packet.answers.forEach(function(e){
-		if(e.type=='PTR'){
+		if(e.type=='PTR'&& e.name!=="_services._dns-sd._udp.local" ){
 			let str=e.data;
 
 			str=str.split('.');
@@ -51,35 +54,65 @@ function parseSRVTXT(packet){
 			e.data.forEach(function(e){
 				txt.push(Buffer.from(e,'utf8'));
 			});		
-			obj.txt=e;
+			obj.TXT=e;
 		}else if(e.type=='A')
 			obj.ipv4=e.data;
 		else if (e.type=='AAAA')
 			obj.ipv6=e.data;
 		
 	});
+	if(obj.SRV===undefined){
+		packet.additionals.forEach(function(e){
+	
+			if(e.type=='SRV'){
+				obj.ttl=e.ttl;
+				obj.SRV=e.data;
+				obj.domain=e.data.target;
+			}
+		});
+	}
+	if(obj.TXT === undefined){
+		
+		let txt=[];
+		packet.additionals.forEach(function(e){
+			
+			if(e.type=='TXT'){
+				e.data.forEach(function(e){
+					txt.push(e.toString('ascii'));
+				});
+			}
+			
+		})		
+		obj.TXT=txt;
 
+	}
 	return JSON.stringify(obj);
+
 }
 
 function main(){
 	let allService=new Set();
 	let mdnsService=new Set();
 	let check=new Set();
-	
+	let temp=new Set();
+
 	mdns.on('response',function(res){
 		res.answers.forEach(function(e){
 			if(e.type=='PTR' && e.name=="_services._dns-sd._udp.local"){
 
 				allService.add(e.data);	
 				if(allService.has(e.data)){
+					console.log("first query"+ e.data);
+					console.log("~~~~");
+					console.log(e);
 					query([e.data,e.data,e.data],['PTR','SRV','TXT']);
+					
 				}
 			}else{
 				let data=parseSRVTXT(res);
 				mdnsService.add(data);
 				let dnssd=JSON.parse(data);
-				
+				console.log(data);
 				if(mdnsService.has(data)&& dnssd.ttl !==undefined && check.has(data) === false){
 					
 					
@@ -87,9 +120,10 @@ function main(){
 
 						try{
 							let i=0;
+							console.log(input);
 							setTimeout(function(){
 								done(input);
-							},input.ttl);
+							},input.ttl*1000);
 						}catch(e){
 							console.log("child process error: "+e);	
 						}
@@ -98,16 +132,39 @@ function main(){
 					thread.send(dnssd).on('message',function(res){
 
 						check.delete(JSON.stringify(res));
-						query([res.service+"local", res.service+"local",res.service+"local"],['PTR','SRV','TXT']);	
+						temp.add(JSON.stringify(res));
+						
+						//console.log("Resend query"+ res.service+"local");
+						query([res.service+"local", res.instance+"."+res.service+"local", res.instance+"."+res.service+"local"],['PTR','SRV','TXT']);	
+						
 						thread.kill();
 
+					}).on('exit',function(){
+			
+						console.log("call exit");
+
+						setTimeout(function(){
+							try{
+								temp.forEach(function(e){
+									if(check.has(e)===false){
+										//console.log("delete\t "+ e );
+										mdnsService.delete(e);
+									}
+								});
+								temp.clear();
+							}catch(error){
+								console.log("temp error: "+ error);
+							}
+						},3000);
 					});
 					check.add(data);
+
 				}
 			}
 		});
 	});
-	query("_services._dns-sd._udp.local","PTR");
+	//query("_services._dns-sd._udp.local","PTR");
+	query(["_test1._udp.local", "Percomlab._test1._udp.local","Percomlab._test1._udp.local" ],["PTR","SRV","TXT"] );
 		
 }
 
