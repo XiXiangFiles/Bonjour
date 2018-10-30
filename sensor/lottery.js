d777280faf96e25236160b8d8fd3e2180f752456
WangWang <luckboystudent@gmail.com>
2018-10-30 14:04:10 +0800

    20181030

---
+++ sensor/lottery.js
@@ -0,0 +1,19 @@
+const Serial=require('serialport');
+const port= new Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_8543034393735181F140-if00',{baudRate : 9600});
+
+port.on('data',function(data){
+	console.log(data);
+});
+class LotteryDevice{
+	function start(){
+		port.write("start",function(err){});
+		return 0;
+	}
+	function ready(){
+		port.write("resdy",function(err){});
+		return 0;
+	}
+}
+
+
+
