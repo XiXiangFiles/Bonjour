aef12391186350621d1048e9446a2885bdfdf89f
WangWang <luckboystudent@gmail.com>
2018-11-02 10:28:24 +0800

    20181102

--- sensor/lottery.js
+++ sensor/lottery.js
@@ -1,20 +1,39 @@
 const Serial=require('serialport');
+const events=require('events');
 const port= new Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_8543034393735181F140-if00',{baudRate : 9600});
 
-/*
-port.on('data',function(data){
-	console.log(data);
-});
-*/
-class LotteryDevice{
-	start(){
+
+function LotteryDevice(){
+		
+	events.EventEmitter.call(this);
+	let device=this;
+
+	this.start=function(){
 		port.write("start",function(err){});
+	}
+	this.ready=function(){
+		port.write("ready",function(err){});
 		return 0;
 	}
-	ready(){
-		port.write("resdy",function(err){});
-		return 0;
+	this.listen=function(){
+		port.on('data',function(data){
+			device.emit('data',data.toString('ascii'));
+		});
 	}
 }
-module.exports=LotteryDevice;
+LotteryDevice.prototype.__proto__=events.EventEmitter.prototype;
+
+/*
+let device= new LotteryDevice();
+device.listen();
+device.on('data',(e)=>{
+	console.log(e);
+})
+setTimeout(function(){
+	device.start();
+},3000);
+*/
+
+module.exports=new LotteryDevice();
+//module.exports=msg;
 
